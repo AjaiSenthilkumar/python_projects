ecc5cf553a222a495003f376b146242b8805cd8e
AjaiSenthilkumar <143886513+AjaiSenthilkumar@users.noreply.github.com>
2023-09-28 13:00:22 +0530

    Add files via upload

---
+++ mablibs.py
@@ -0,0 +1,17 @@
+#practice projects of madlibs
+
+youtuber = ""
+
+#print the subscribe to and youtuuber
+#print("Subscribe to "+youtuber)
+#print("subscribe to {}".format (youtuber))
+#print(f"subscribe to {youtuber}") #clean way of string concatination
+
+adj = input("Adjective: ")
+verb1 = input("verb1: ")
+verb2 = input("verb2: ")
+famous_person = input("famous Person :")
+matlibs = f"Commputer programming is so {adj}! It makes me so excided all the thime because\
+    I love to {verb1}.stay hydrated and {verb2} like you are {famous_person}"
+
+print(matlibs)
